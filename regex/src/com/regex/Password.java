93d8fd0d6dcbdfba7fd19c7fbb76c1ae8a551e60
PAVAN903 <navap903@gmail.com>
2023-01-05 13:10:19 +0530

    validating password with one upper case

---
+++ regex/src/com/regex/Password.java
@@ -0,0 +1,20 @@
+package com.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Password {
+
+	public static void main(String[] args) {
+		System.out.println("enter valid password");
+		Scanner sc=new Scanner(System.in);
+		String Upassword=sc.nextLine();
+		Pattern pat=Pattern.compile("^[A-Z]{1}[a-z]{1,}$");
+		Matcher mat=pat.matcher(Upassword);
+		boolean b=mat.matches();
+		System.out.println(b);
+
+	}
+
+}
